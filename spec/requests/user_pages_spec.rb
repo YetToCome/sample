3fff019c7a29bebe6ef55b6ea088627fe95d182e
YetToCome <wjw15129@gmail.com>
2014-02-15 11:47:44 +0800

    Make a basic User model (including secure passwords)

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,13 +1,40 @@
 require 'spec_helper'
 
-describe "User pages" do
+describe User do
 
-  subject { page }
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
 
-  describe "signup page" do
-    before { visit signup_path }
+  subject { @user }
 
-    it { should have_content('Sign up') }
-    it { should have_title(full_title('Sign up')) }
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+
+  it { should be_valid }
+
+  describe "when name is not present" do
+    before { @user.name = " " }
+    it { should_not be_valid }
+  end
+  describe "when email format is invalid" do
+    it "should be invalid" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+                     foo@bar_baz.com foo@bar+baz.com]
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        expect(@user).not_to be_valid
+      end
+    end
+  end
+
+  describe "when email format is valid" do
+    it "should be valid" do
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        expect(@user).to be_valid
+      end
+    end
   end
 end
